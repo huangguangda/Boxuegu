16c487a27f812b7cacbe578009c5933964b87979
huangguangda <2397923107@qq.com>
2018-07-19 01:16:45 +0800

    教育系统APP(八)

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SettingActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SettingActivity.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import cn.edu.gdmec.android.boxuegu.R;
 import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
 
